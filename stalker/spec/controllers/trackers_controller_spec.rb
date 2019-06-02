b8a7c7ea64ad215561cb0b7f21f9ad0dfc4bcc3f
wehmoreira <wehmoreira@gmail.com>
2019-06-02 13:18:05 -0300

    stalker: gerando controller Trackers

---
+++ stalker/spec/controllers/trackers_controller_spec.rb
@@ -0,0 +1,14 @@
+# Mantive aqui os testes inalterados ao gerar o controller. Preferi utilizar os
+# request specs para testar o controller, seguindo recomendação da equipe do RSpec
+require 'rails_helper'
+
+RSpec.describe TrackersController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
