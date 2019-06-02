d5e09fbdefc616b8f572284fcda4ffe9032d3c45
wehmoreira <wehmoreira@gmail.com>
2019-06-02 11:37:49 -0300

    stalker: incluindo validacoes de guid, url e datetime no modelo Tracker + testes

--- stalker/app/models/tracker.rb
+++ stalker/app/models/tracker.rb
@@ -1,2 +1,14 @@
 class Tracker < ApplicationRecord
+  require 'uri'
+  validates :guid, presence: true, format: { with: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, message: 'guid inválido' }
+  validates :url, presence: true
+  validates :datetime, presence: true, format: { with: /\d{4}-\d\d?-\d\d? \d\d?:\d\d?:\d\d?/ }
+  validate :invalid_url
+
+  def invalid_url
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    errors.add(:url, 'url inválida')
+  end
 end
--- stalker/spec/models/tracker_spec.rb
+++ stalker/spec/models/tracker_spec.rb
@@ -2,15 +2,42 @@ require 'rails_helper'
 
 RSpec.describe Tracker, type: :model do
   context 'validação' do
-    let(:tracker) { Tracker.new.save }
-    it 'deve conter o campo `guid`' do
-      expect(tracker).to eq(false)
+    subject do
+      described_class.new(
+        guid: "aa5bbb17-cdb9-4618-abfa-db6dd2e00844", url: "http://teste.com/home",
+        datetime: "2019-5-31 17:34:49"
+      )
     end
-    it 'deve conter o campo `url`' do
-      expect(tracker).to eq(false)
+    let(:saved) { subject.save }
+    context 'guid' do
+      it 'deve conter o campo `guid`' do
+        subject.guid = nil
+        expect(saved).to eq(false)
+      end
+      it 'valida a formatação de um guid' do
+        subject.guid = "Lorem ipsum dolor sit amet"
+        expect(saved).to eq(false)
+      end
     end
-    it 'deve conter o campo `datetime`' do
-      expect(tracker).to eq(false)
+    context 'url' do
+      it 'deve conter o campo `url`' do
+        subject.url = nil
+        expect(saved).to eq(false)
+      end
+      it 'valida a formatação de um url' do
+        subject.url = "Lorem ipsum dolor sit amet"
+        expect(saved).to eq(false)
+      end
+    end
+    context 'datetime' do
+      it 'deve conter o campo `datetime`' do
+        subject.datetime = nil
+        expect(saved).to eq(false)
+      end
+      it 'valida a formatação de data e hora' do
+        subject.datetime = "Lorem ipsum dolor sit amet"
+        expect(saved).to eq(false)
+      end
     end
   end
 end
