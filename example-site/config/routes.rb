30b80343b702d5e00e12fafe911a5aaec60a9d59
wehmoreira <wehmoreira@gmail.com>
2019-06-04 14:38:24 -0300

    example-site: acertando rotas para home, contato e sobre

--- example-site/config/routes.rb
+++ example-site/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :contacts, except: [ :edit, :show, :update, :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/home')
+  resources :contacts, except: [ :edit, :show, :update, :destroy ]
   get '/home', to: 'contacts#index'
   get '/contato', to: 'contacts#new'
   get '/sobre', to: 'contacts#sobre'
-  root 'contacts#index'
 end
