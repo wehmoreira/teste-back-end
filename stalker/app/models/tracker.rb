3077b8a816f147b8020c3c2363be84e1aaabf4cf
wehmoreira <wehmoreira@gmail.com>
2019-06-05 10:19:43 -0300

    stalker: refatorando validador de url no modelo Tracker

--- stalker/app/models/tracker.rb
+++ stalker/app/models/tracker.rb
@@ -3,11 +3,11 @@ class Tracker < ApplicationRecord
   validates :guid, presence: true, format: { with: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, message: 'guid inválido' }
   validates :url, presence: true
   validates :datetime, presence: true, format: { with: /\d{4}-\d\d?-\d\d? \d\d?:\d\d?:\d\d?/ }
-  validate :invalid_url
+  validate :url_format
 
-  def invalid_url
+  def url_format
     uri = URI.parse(url)
-    uri.is_a?(URI::HTTP) && !uri.host.nil?
+    raise URI::InvalidURIError unless uri.is_a?(URI::HTTP) && !uri.host.nil?
   rescue URI::InvalidURIError
     errors.add(:url, 'url inválida')
   end
